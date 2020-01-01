ae1b59fdf34cfe47a3735feae7b01bde47ee3e1e
Vivekanand <vivekanand.rao@gmail.com>
2020-01-01 12:21:53 -0800

    Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 import Home from './components/Home';
+import Game from './components/Game';
+import HighScores from './components/HighScores';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <div className="container">
+        <Route exact path="/" component={Home}/>
+        <Route path="/game" component={Game}/>
+        <Route path="/highScores" component={HighScores}/>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <h1>Quiz App</h1>
-      <a href="/game" className="btn">Start Game</a>
-      <a href="/highScores" className="btn">High Scores</a>
-    </div>
+      <Link to="/game" className="btn">Start Game</Link>
+      <Link to="/highScores" className="btn">High Scores</Link>
+    </>
   )
 }
