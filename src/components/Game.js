aefbf0965fba60e6bbe5de9f25b598d504832314
Vivekanand <vivekanand.rao@gmail.com>
2020-01-01 12:40:08 -0800

    Displaying Questions

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import Question from './Question';
+
+const sampleQuestion = {
+  question: "Who is making the Web standards?",
+  answerChoices: [
+    'Microsoft',
+    'Google',
+    'The World Wide Web Consortium',
+    'Mozilla'
+  ]
+};
 
 export default class Game extends Component {
   render() {
     return (
       <>
-        <h1>Game</h1>
+        <Question question={sampleQuestion}/>
       </>
     )
   }
