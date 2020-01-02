7a9618c807a45c07ac6d4a7f0d0010e22a2dadb1
Vivekanand <vivekanand.rao@gmail.com>
2020-01-01 18:29:30 -0800

    Helpers

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Question from './Question';
+import { loadQuestions } from "../helpers/QuestionsHelper";
 
 export default class Game extends Component {
   constructor(props){
@@ -10,19 +11,8 @@ export default class Game extends Component {
     };
   }
   async componentDidMount() {
-    const url = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple';
     try {
-      const res = await fetch(url);
-      const { results } = await res.json();
-      const questions = results.map(loadedQuestion => {
-        const formattedQuestion = {
-          question: loadedQuestion.question,
-          answerChoices: [...loadedQuestion.incorrect_answers]
-        };
-        formattedQuestion.answer = Math.floor(Math.random() * 4);
-        formattedQuestion.answerChoices.splice(formattedQuestion.answer, 0, loadedQuestion.correct_answer);
-        return formattedQuestion;
-      });
+      const questions = await loadQuestions();
       this.setState({
         questions: questions,
         currentQuestion: questions[0]
