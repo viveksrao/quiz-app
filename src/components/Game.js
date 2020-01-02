e7d8b2a9860e1c68ba82c6632364ac3aa7a2b45d
Vivekanand <vivekanand.rao@gmail.com>
2020-01-01 19:10:55 -0800

    Changing Questions

--- src/components/Game.js
+++ src/components/Game.js
@@ -15,13 +15,29 @@ export default class Game extends Component {
     try {
       const questions = await loadQuestions();
       this.setState({
-        questions: questions,
-        currentQuestion: questions[0],
-        loading: false
+        questions: questions
+      }, () => {
+        this.changeQuestion();
       });
     } catch (error) {
       console.error(error);
     }
+  }
+
+  changeQuestion = () => {
+    // get a random index of a question
+    const randomQuestionIndex = Math.floor(Math.random() * this.state.questions.length)
+    // we set the current question to the question at that random index
+    const currentQuestion = this.state.questions[randomQuestionIndex];
+    // remove that question from the questions going forward
+    const remainingQuestions = [...this.state.questions];
+    remainingQuestions.splice(randomQuestionIndex, 1);
+    // update the state to reflect these changes
+    this.setState({
+      questions: remainingQuestions,
+      currentQuestion: currentQuestion,
+      loading: false
+    });
   }
   
   render() {
