5bf0e90da493e767ba35df7492242a6ae6ea5db2
Vivekanand <vivekanand.rao@gmail.com>
2020-01-01 17:48:48 -0800

    Connecting to Open Trivia DB

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react'
 import Question from './Question';
 
-const sampleQuestion = {
-  question: "Who is making the Web standards?",
-  answerChoices: [
-    'Microsoft',
-    'Google',
-    'The World Wide Web Consortium',
-    'Mozilla'
-  ]
-};
-
 export default class Game extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      questions: null,
+      currentQuestion: null
+    };
+  }
+  async componentDidMount() {
+    const url = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple';
+    try {
+      const res = await fetch(url);
+      const { results } = await res.json();
+      const questions = results.map(loadedQuestion => {
+        const formattedQuestion = {
+          question: loadedQuestion.question,
+          answerChoices: [...loadedQuestion.incorrect_answers]
+        };
+        formattedQuestion.answer = Math.floor(Math.random() * 4);
+        formattedQuestion.answerChoices.splice(formattedQuestion.answer, 0, loadedQuestion.correct_answer);
+        return formattedQuestion;
+      });
+      this.setState({
+        questions: questions,
+        currentQuestion: questions[0]
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
   render() {
     return (
       <>
-        <Question question={sampleQuestion}/>
+        {this.state.currentQuestion && (
+          <Question question={this.state.currentQuestion}/>
+        )}
       </>
     )
   }
